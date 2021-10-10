ae80f21857337d397f649889bd589cfaec70af92
Piotrek <piotr.chrzanowski@>
2021-10-10 11:42:22 +0200

    Added run function which is responsible for state transitions

--- src/finiteStateMachine.h
+++ src/finiteStateMachine.h
@@ -8,28 +8,22 @@
 
 #pragma once
 #include <functional>
-
-
-template <class State, class Event>
-struct State_t
-{
-    State state;
-    std::function<Event()> handler;
-};
+#include <map>
+#include <iostream>
 
 template <class State, class Event>
 struct Transition_t
 {
-    State_t<State, Event> currentState;
-    State_t<State, Event> nextState;
+    State currentState;
     Event event;
+    State nextState;
 };
 
 template <class State, class Event>
 class FSM
 {
 public:
-    FSM(std::vector<Transition_t<State, Event>> transitionsTable_, bool continousRun_ = false);
+    FSM(const std::vector<Transition_t<State, Event>> transitionsTable_, const std::map<State, std::function<Event()>> stateHandlers_, bool continousRun_ = false);
     virtual ~FSM() = default;
 
     State GetState();
@@ -37,7 +31,41 @@ public:
 
 private:
     std::vector<Transition_t<State, Event>> transitionsTable;
-    State_t<State, Event> currentState;
+    std::map<State, std::function<Event()>> stateHandlers;
+    State currentState;
     bool continousRun;
 };
 
+template <class State, class Event>
+FSM<State, Event>::FSM(const std::vector<Transition_t<State, Event>> transitionsTable_, const std::map<State, std::function<Event()>> stateHandlers_, bool continousRun_) : transitionsTable(transitionsTable_), stateHandlers(stateHandlers_), continousRun(continousRun_)
+{
+    currentState = transitionsTable.at(0).currentState;
+}
+
+template <class State, class Event>
+State FSM<State, Event>::GetState()
+{
+    return currentState;
+}
+
+template <class State, class Event>
+void FSM<State, Event>::Run()
+{
+    do
+    {
+        Event newEvent = stateHandlers[currentState]();
+
+        //Search through vector table
+        for (auto &transition : transitionsTable)
+        {
+            if (transition.event == newEvent) {
+                if (transition.currentState == currentState)
+                {
+                    currentState = transition.nextState;
+                }
+            }
+        }
+
+    } while (continousRun);
+    
+}
--- test/finiteStateMachineTests.cpp
+++ test/finiteStateMachineTests.cpp
@@ -1,2 +1,90 @@
-#include "gtest/gtest.h"
 #include "finiteStateMachine.h"
+#include "gtest/gtest.h"
+#include <map>
+#include <memory>
+#include <vector>
+#include <iostream>
+
+enum class Event
+{
+    Transition_To_S1,
+    Transition_To_S2,
+    Transition_To_S3,
+    Transition_To_S4
+};
+
+enum class State
+{
+    S1,
+    S2,
+    S3,
+    S4
+};
+
+Event s1_handler()
+{
+    std::cout << "S1 Handler" << std::endl;
+    return Event::Transition_To_S2;
+}
+
+Event s2_handler()
+{
+    std::cout << "S2 Handler" << std::endl;
+    return Event::Transition_To_S3;
+}
+
+Event s3_handler()
+{
+    std::cout << "S3 Handler" << std::endl;
+    return Event::Transition_To_S4;
+}
+
+Event s4_handler()
+{
+    std::cout << "S4 Handler" << std::endl;
+    return Event::Transition_To_S1;
+}
+
+std::map<State, std::function<Event()>> stateHandlers =
+    {
+        {State::S1, std::bind(s1_handler)},
+        {State::S2, std::bind(s2_handler)},
+        {State::S3, std::bind(s3_handler)},
+        {State::S4, std::bind(s4_handler)}};
+
+std::vector<Transition_t<State, Event>> transitionTable =
+    {
+        {State::S1, Event::Transition_To_S2, State::S2},
+        {State::S2, Event::Transition_To_S3, State::S3},
+        {State::S3, Event::Transition_To_S4, State::S4},
+        {State::S4, Event::Transition_To_S1, State::S1}};
+
+class FSM_Tests : public ::testing::Test
+{
+public:
+    FSM_Tests()
+    {
+    }
+
+    void SetUp()
+    {
+        finiteStateMachine = std::make_shared<FSM<State, Event>>(transitionTable, stateHandlers, false);
+    }
+
+public:
+    std::shared_ptr<FSM<State, Event>> finiteStateMachine;
+};
+
+
+TEST_F(FSM_Tests, FSM_Should_Init_In_First_State)
+{
+    EXPECT_EQ(finiteStateMachine->GetState(), State::S1);
+}
+
+TEST_F(FSM_Tests, FSM_Should_Change_State_When_Event_Occured)
+{
+    EXPECT_EQ(finiteStateMachine->GetState(), State::S1);
+    finiteStateMachine->Run();
+    EXPECT_EQ(finiteStateMachine->GetState(), State::S2);
+}
+
